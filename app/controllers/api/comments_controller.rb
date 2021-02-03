15034b56540a8dcb9d764c7c9a6e40ae48391045
Ehugo2000 <erik.hgsn@gmail.com>
2021-02-03 16:46:25 +0100

    Updating Create action in comments_controller.

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -2,7 +2,19 @@ class Api::CommentsController < ApplicationController
   def index; end
 
   def create
-    comment = Comment.create
-    render json: { comment: comment }, status: 201
+    comment = Comment.create(comment_params)
+    if comment.persisted?
+      binding.pry
+      render json: { comment: 'Your comment where posted successfully!' }, status: 201
+    else
+      binding.pry
+      render json: { message: comment.errors.full_message.to_sentence }, status: 422
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
--- spec/requests/api/visitor_can_leave_a_comment_spec.rb
+++ spec/requests/api/visitor_can_leave_a_comment_spec.rb
@@ -1,19 +1,23 @@
 RSpec.describe 'POST /api/articles/:article_id/comments', type: :request do
-  let!(:existing_article) { create(:article)}
+  let!(:existing_article) { create(:article) }
 
   describe 'successfully create a comment' do
     before do
       post "/api/articles/#{existing_article.id}/comments",
-      params: {
-        comment: {
-          body: "A comment",
-          article_id: existing_article.id,
-        }
-      }
+           params: {
+             comment: {
+               body: 'A comment',
+               article_id: existing_article.id
+             }
+           }
     end
-    
+
     it 'is expected to return a 201 status' do
       expect(response).to have_http_status 201
-   end
+    end
+
+    it 'is expected to return a comment to an article' do
+      expect(response_json['comment']['body']).to eq 'A comment'
+    end
   end
 end
