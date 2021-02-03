4659d52ce296e1df70f6c0e4fcf81657be6e5441
Ehugo2000 <erik.hgsn@gmail.com>
2021-02-03 16:01:46 +0100

    Adding association between article and cpmments.

--- db/migrate/20210203144424_create_comments.rb
+++ db/migrate/20210203144424_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.string :body
-
+      t.references :articles, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -24,8 +24,10 @@ ActiveRecord::Schema.define(version: 2021_02_03_144424) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
+    t.bigint "articles_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["articles_id"], name: "index_comments_on_articles_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +59,5 @@ ActiveRecord::Schema.define(version: 2021_02_03_144424) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "comments", "articles", column: "articles_id"
 end
